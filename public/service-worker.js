718a47ae3a1be78c1401cf94316003df293cb041
Bingdev <bingdev505@gmail.com>
2025-11-10 06:38:40 +0000

    when i cloased the app, if it works background and send the notification

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,65 @@
+const CACHE_NAME = 'financeflow-cache-v1';
+const urlsToCache = [
+  '/',
+  '/overview',
+  '/ai-chat',
+  '/tasks',
+  '/passwords',
+  '/business',
+  '/reports',
+  '/settings',
+  '/login',
+  '/signup',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('push', event => {
   const data = event.data.json();
+  const title = data.title || 'FinanceFlow';
   const options = {
     body: data.body,
-    icon: '/icon.png',
+    icon: '/icons/icon-192x192.png',
+    badge: '/icons/icon-192x192.png'
   };
-  event.waitUntil(
-    self.registration.showNotification(data.title, options)
-  );
+  event.waitUntil(self.registration.showNotification(title, options));
 });
