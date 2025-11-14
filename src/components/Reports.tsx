9649b17ad40da564acd078dbf82473842a543d82
Bingdev <bingdev505@gmail.com>
2025-11-14 10:26:41 +0000

    how to make resposive in mobile this table

--- src/components/Reports.tsx
+++ src/components/Reports.tsx
@@ -207,7 +207,7 @@ export default function Reports() {
              </Select>
           </div>
 
-          <div className="border rounded-md">
+          <div className="border rounded-md overflow-x-auto">
             <Table>
                 <TableHeader>
                     <TableRow>
